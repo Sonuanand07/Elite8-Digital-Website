e2f4a9be9ace92c78b03b4ba51428cb9ef81296b
Sonu Anand <sanand@bizaitek.com>
2025-07-19 00:16:55 +0530

    Add Changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: [
-//       "images.unsplash.com",
-//       "images.pexels.com",
-//       "media.giphy.com"
-//     ],
-//   },
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "images.pexels.com",
+      "media.giphy.com"
+    ],
+  },
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
